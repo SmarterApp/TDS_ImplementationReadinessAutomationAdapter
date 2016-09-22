3350103c77f5d9719e2d85052edf661571932d4d
Stefan Eng <stefaneng@g.ucla.edu>
2016-09-22 13:21:03 -0700

    Add SbossStudentTest created successfully

--- irp-automation-adapter-sboss-engine/src/test/java/org/cresst/sb/irp/automation/adapter/student/SbossStudentTest.java
+++ irp-automation-adapter-sboss-engine/src/test/java/org/cresst/sb/irp/automation/adapter/student/SbossStudentTest.java
@@ -1,19 +1,30 @@
 package org.cresst.sb.irp.automation.adapter.student;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
+import java.net.URL;
+
+import org.cresst.sb.irp.automation.adapter.web.AutomationRestTemplate;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 public class SbossStudentTest {
 
+    @Mock private AutomationRestTemplate mockStudentRestTemplate;
+    private URL url;
+
     @Before
     public void setUp() throws Exception {
+        url = new URL("https://server.test");
+        mockStudentRestTemplate = mock(AutomationRestTemplate.class);
     }
 
     @Test
-    public void checkApproval() {
-        assertTrue(true);
+    public void SbossStudentCreate() {
+        Student testStudent = new SbossStudent(mockStudentRestTemplate, url);
+        assertNotNull(testStudent);
     }
 
 }
