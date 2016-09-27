9923b4ce8743512cd51a9e007a68f2714eaa0212
Stefan Eng <stefaneng@g.ucla.edu>
2016-09-27 11:13:20 -0700

    IRP-162: generate xml response for updateResponses

--- irp-automation-adapter-sboss-engine/src/test/java/org/cresst/sb/irp/automation/adapter/student/UpdateResponsesBuilderTest.java
+++ irp-automation-adapter-sboss-engine/src/test/java/org/cresst/sb/irp/automation/adapter/student/UpdateResponsesBuilderTest.java
@@ -1,21 +1,36 @@
 package org.cresst.sb.irp.automation.adapter.student;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
+import java.io.File;
+
+import org.cresst.sb.irp.automation.adapter.student.data.PageContents;
 import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.Document;
 
 public class UpdateResponsesBuilderTest {
-    UpdateResponsesBuilder updateResponsesBuilder;
+    private StudentResponseService responseService;
+    private final String studentTestData = "ItemID Response\n1 <![CDATA[a]]\n" +
+            "2700 <![CDATA[b]]\n" +
+            "2701 <![CDATA[c]]\n" +
+            "2702 <![CDATA[test]]";
+    private PageContents pageContents;
 
     @Before
     public void setUp() throws Exception {
-        updateResponsesBuilder = new UpdateResponsesBuilder();
+        // Load the sample page contents response
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("getPageContent_sample_response.xml").getFile());
+        pageContents = new PageContents(file);
+
+        responseService = new StudentResponseService(studentTestData);
     }
 
     @Test
-    public void UpdateResponsesBuilder() {
-        assertNotNull(updateResponsesBuilder);
+    public void createRequest_WithRandomResponse() {
+        Document request = UpdateResponsesBuilder.createRequest(responseService, "![CDATA[language:ENU;Other:TDS_Other;Print Size:TDS_PS_L3]]", pageContents);
+        assertNotNull(request);
     }
 
 }
