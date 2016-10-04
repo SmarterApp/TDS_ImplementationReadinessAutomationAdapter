4c4b8fe3397b8061937414a4c5480eba24ffe7e8
Stefan Eng <stefaneng@g.ucla.edu>
2016-10-04 09:19:08 -0700

    IRP-159: Use env variable for student url to run login test

--- irp-automation-adapter-sboss-engine/src/test/java/org/cresst/sb/irp/automation/adapter/student/StudentLoginTest.java
+++ irp-automation-adapter-sboss-engine/src/test/java/org/cresst/sb/irp/automation/adapter/student/StudentLoginTest.java
@@ -1,12 +1,11 @@
 package org.cresst.sb.irp.automation.adapter.student;
 
 import org.cresst.sb.irp.automation.adapter.web.AutomationRestTemplate;
-import org.junit.Ignore;
+import org.cresst.sb.irp.automation.adapter.web.SbossAutomationRestTemplate;
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.net.URL;
 
 import static org.junit.Assert.assertTrue;
@@ -16,20 +15,29 @@ import static org.junit.Assert.assertTrue;
  @author Ernesto De La Luz Martinez
  */
 
-@Ignore("TDS Server is IP restricted. Remove Ignore when machine has access to sever.")
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration( locations = { "classpath*:root-context.xml"})
 public class StudentLoginTest {
 	private final static Logger logger = LoggerFactory.getLogger(StudentLoginTest.class);
+	private final static String ENV_STUDENT_URL = "TDS_STUDENT_URL";
 
-    @Mock
-    AutomationRestTemplate mockStudentRestTemplate;
+    AutomationRestTemplate studentRestTemplate;
+    Student studentLogin;
+
+    @Before
+    public void setup() throws Exception {
+        URL studentUrl = null;
+        try {
+            studentUrl = new URL(System.getenv(ENV_STUDENT_URL));
+        } catch (NullPointerException e) {
+            logger.error("Environment variable " + ENV_STUDENT_URL + " not set.");
+        }
+        studentRestTemplate = new SbossAutomationRestTemplate();
+        studentLogin = new SbossStudent(studentRestTemplate, studentUrl, null);
+    }
 
 	@Test
 	public void login() throws Exception {
-
-		Student studentLogin = new SbossStudent(mockStudentRestTemplate, new URL("http://test.server/student"), null);
-
 		boolean loginSuccessful = studentLogin.login("GUEST Session", "ID:GUEST;FirstName:GUEST", "");
 
         assertTrue(loginSuccessful);
