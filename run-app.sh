b0678fd31584dc0cd78e92b2e17a84962bdce144
Paul Espinosa <paulespin@gmail.com>
2016-11-10 13:23:10 -0800

    Updated the run script so the Adapter runs on port 8181 to avoid conflict with IRP running on port 8080

--- run-app.sh
+++ run-app.sh
@@ -1,4 +1,4 @@
 source env.sh
 cd irp-automation-adapter-sboss-application
-mvn $@ spring-boot:run
+mvn -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 $@ spring-boot:run
 cd - 
