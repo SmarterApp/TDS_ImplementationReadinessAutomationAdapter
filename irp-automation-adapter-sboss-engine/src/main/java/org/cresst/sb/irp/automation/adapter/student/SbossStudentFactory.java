6e89065e2a979123304ed6afbda8f477fa8c11ca
Stefan Eng <stefaneng@ucla.edu>
2016-11-18 15:15:48 -0800

    CP-227, CP-159: Map student to eligible tests

--- irp-automation-adapter-sboss-engine/src/main/java/org/cresst/sb/irp/automation/adapter/student/SbossStudentFactory.java
+++ irp-automation-adapter-sboss-engine/src/main/java/org/cresst/sb/irp/automation/adapter/student/SbossStudentFactory.java
@@ -1,19 +1,29 @@
 package org.cresst.sb.irp.automation.adapter.student;
 
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.google.common.collect.Lists;
 import org.cresst.sb.irp.automation.adapter.web.AutomationRestTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.retry.RetryOperations;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class SbossStudentFactory implements StudentFactory {
     private final static Logger logger = LoggerFactory.getLogger(SbossStudentFactory.class);
-
+    
     private final AutomationRestTemplate studentRestTemplate;
     private final URL studentUrl;
     private final StudentResponseGenerator studentResponseGenerator;
@@ -29,9 +39,73 @@ public class SbossStudentFactory implements StudentFactory {
         this.studentResponseGenerator = studentResponseGenerator;
         this.retryTemplate = retryTemplate;
     }
+    
+    private List<TestStudentMapping> parseTestStudentMapping(InputStream inputStream) {
+        CsvMapper mapper = new CsvMapper();
+        CsvSchema schema = mapper.schemaFor(TestStudentMapping.class).withHeader();
+
+        try {
+            MappingIterator<TestStudentMapping> it = mapper.readerFor(TestStudentMapping.class)
+                    .with(schema)
+                    .readValues(inputStream);
+            return it.readAll();
+        } catch (IOException e) {
+            logger.error("Error parsing csv for TestStudentMapping, {}", e.getMessage());
+        }
+        return null;
+    }
+    
+    private List<AutomationStudent> loadStudentsFromMapping(InputStream csvInputStream) {
+    	List<AutomationStudent> students = new ArrayList<>();
+        
+    	List<TestStudentMapping> testStudentMappings = parseTestStudentMapping(csvInputStream);
+		Map<String, List<TestStudentMapping>> groupedStudents = new HashMap<>();
+		for(TestStudentMapping t : testStudentMappings) {
+			String studentName = t.getStudentName();
+			if(groupedStudents.containsKey(studentName)) {
+				groupedStudents.get(studentName).add(t);
+			} else {
+				List<TestStudentMapping> newGroup = new ArrayList<>();
+				newGroup.add(t);
+				groupedStudents.put(studentName, newGroup);
+			}
+		}
+		
+		// Create SbossStudents for each of the tests a student needs to take  
+		for(String studentName : groupedStudents.keySet()) {
+			List<TestStudentMapping> studentTests = groupedStudents.get(studentName);
+			Map<StudentTestTypeEnum, String> testNames = new HashMap<>();
+			 String altId = null;
+			 for(TestStudentMapping m : studentTests) {
+				 if(m.getTestType().toLowerCase().equals("combined")) break;
+				 
+				 altId = m.getAlternateSSID();
+				 if(m.isCat()) {
+					 testNames.put(StudentTestTypeEnum.CAT, m.getTest());
+				 } else {
+					 testNames.put(StudentTestTypeEnum.FIXED, m.getTest());
+				 }
+			 }
+			 
+			 SbossStudent student =  new SbossStudent(
+		                studentRestTemplate,
+		                retryTemplate,
+		                studentUrl,
+		                testNames,
+		                studentResponseGenerator,
+		                altId,
+		                studentName);
+			 
+			 students.add(student);
+		}
+		
+		return students;
+    }
 
     @Override
     public List<AutomationStudent> createStudents() {
+    	List<AutomationStudent> students = new ArrayList<>();
+    	
         Map<StudentTestTypeEnum, String> testNames = new HashMap<>();
         testNames.put(StudentTestTypeEnum.FIXED, "(SBAC_PT)IRP-Perf-ELA-3-Summer-2015-2016");
         testNames.put(StudentTestTypeEnum.CAT, "(SBAC_PT)SBAC-IRP-CAT-ELA-3-Summer-2015-2016");
@@ -44,7 +118,9 @@ public class SbossStudentFactory implements StudentFactory {
                 studentResponseGenerator,
                 "AIRP39990001",
                 "IRPStudentA");
-
-        return Lists.<AutomationStudent>newArrayList(student);
+        
+        students.add(student);
+				
+		return students;
     }
 }
---
+++ irp-automation-adapter-sboss-engine/src/main/java/org/cresst/sb/irp/automation/adapter/student/TestStudentMapping.java
@@ -0,0 +1,77 @@
+package org.cresst.sb.irp.automation.adapter.student;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"test", "testType","segmentName","componentTestName","cat","studentSSID", "alternateSSID", "studentName"})
+public class TestStudentMapping {
+	private String test;
+	private String testType;
+	private String segmentName;
+	private String componentTestName;
+	private String studentSSID;
+	private String alternateSSID;
+	private String studentName;
+	private String cat;
+	private boolean isCat;
+	@Override
+	public String toString() {
+		return "TestStudentMapping [test=" + test + ", testType=" + testType + ", segmentName=" + segmentName
+				+ ", componentTestName=" + componentTestName + ", studentSSID=" + studentSSID + ", alternateSSID="
+				+ alternateSSID + ", studentName=" + studentName + ", isCat=" + isCat + "]";
+	}
+	
+	public boolean isCat() {
+		return isCat;
+	}
+	
+	public String getCat() {
+		return cat;
+	}
+	public void setCat(String cat) {
+		this.isCat = cat.toLowerCase().equals("true");
+		this.cat = cat;
+	}
+	public String getTest() {
+		return test;
+	}
+	public void setTest(String test) {
+		this.test = test;
+	}
+	public String getTestType() {
+		return testType;
+	}
+	public void setTestType(String testType) {
+		this.testType = testType;
+	}
+	public String getSegmentName() {
+		return segmentName;
+	}
+	public void setSegmentName(String segmentName) {
+		this.segmentName = segmentName;
+	}
+	public String getComponentTestName() {
+		return componentTestName;
+	}
+	public void setComponentTestName(String componentTestName) {
+		this.componentTestName = componentTestName;
+	}
+	public String getStudentSSID() {
+		return studentSSID;
+	}
+	public void setStudentSSID(String studentSSID) {
+		this.studentSSID = studentSSID;
+	}
+	public String getAlternateSSID() {
+		return alternateSSID;
+	}
+	public void setAlternateSSID(String alternateSSID) {
+		this.alternateSSID = alternateSSID;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+}
