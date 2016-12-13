6af5e03d2ed0f9e5bc4e0e8e0f2e119d42e14bf7
Paul Espinosa <paulespin@gmail.com>
2016-12-13 04:16:56 +0000

    bitbucket_beanstalk_deploy.sh edited online with Bitbucket

--- bitbucket_beanstalk_deploy.sh
+++ bitbucket_beanstalk_deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-export APP_VERSION=$BITBUCKET_COMMIT
-pip install awscli
+export APP_VERSION=`git rev-parse --short HEAD`
+echo $APP_VERSION
+export APP_NAME_HYPHEN=`echo $APP_NAME | tr " " -`
+echo $APP_NAME_HYPHEN
 
 # clean build artifacts and create the application archive (also ignore any files named .git* in any folder)
 git clean -fd
@@ -9,14 +11,14 @@ git clean -fd
 # precompile assets, ...
 
 # move and rename the application to a versioned WAR file
-mv irp-automation-adapter-sboss-application/target/irp-automation-adapter-sboss-application-*.war  "${APP_NAME}-${APP_VERSION}.war"
+mv irp-automation-adapter-sboss-application/target/irp-automation-adapter-sboss-application-*.war "${APP_NAME_HYPHEN}-${APP_VERSION}.war"
 
 # delete any version with the same name (based on the short revision)
 aws elasticbeanstalk delete-application-version --application-name "${APP_NAME}" --version-label "${APP_VERSION}"  --delete-source-bundle
 
 # upload to S3
-aws s3 cp ${APP_NAME}-${APP_VERSION}.war s3://${S3_BUCKET}/${APP_NAME}-${APP_VERSION}.war
+aws s3 cp ${APP_NAME_HYPHEN}-${APP_VERSION}.war s3://${S3_BUCKET}/${APP_NAME_HYPHEN}-${APP_VERSION}.war
 
 # create a new version and update the environment to use this version
-aws elasticbeanstalk create-application-version --application-name "${APP_NAME}" --version-label "${APP_VERSION}" --source-bundle S3Bucket="${S3_BUCKET}",S3Key="${APP_NAME}-${APP_VERSION}.war"
+aws elasticbeanstalk create-application-version --application-name "${APP_NAME}" --version-label "${APP_VERSION}" --source-bundle S3Bucket="${S3_BUCKET}",S3Key="${APP_NAME_HYPHEN}-${APP_VERSION}.war"
 aws elasticbeanstalk update-environment --environment-name "${ENV_NAME}" --version-label "${APP_VERSION}"
