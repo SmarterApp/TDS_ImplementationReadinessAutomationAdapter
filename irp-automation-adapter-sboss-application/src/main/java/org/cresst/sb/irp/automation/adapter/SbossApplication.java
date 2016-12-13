b2f7230d0510f0ad84041bd2822226864441574f
Paul Espinosa <paulespin@gmail.com>
2016-12-13 11:12:30 -0800

    CP-201: Turned the application into a deployable WAR.

--- irp-automation-adapter-sboss-application/src/main/java/org/cresst/sb/irp/automation/adapter/SbossApplication.java
+++ irp-automation-adapter-sboss-application/src/main/java/org/cresst/sb/irp/automation/adapter/SbossApplication.java
@@ -2,11 +2,18 @@ package org.cresst.sb.irp.automation.adapter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource(locations = { "classpath:application-context.xml" })
-public class SbossApplication {
+public class SbossApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SbossApplication.class);
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SbossApplication.class, args);
