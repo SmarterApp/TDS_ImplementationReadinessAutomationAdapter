bc2a6edcd3597ae741a58953b935741327f52fe9
Paul Espinosa <paulespin@gmail.com>
2016-12-12 22:44:32 -0800

    CP-201: Removed extra server port declarations in run-app.sh. Updating application pom with spring boot plugin and configuring it to be deployable to tomcat. Removed logback files since they aren’t being used. The log file will be set in an environment variable.

--- run-app.sh
+++ run-app.sh
@@ -1,4 +1,4 @@
 source env.sh
 cd irp-automation-adapter-sboss-application
-mvn -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 -Dserver.port=8181 $@ spring-boot:run
+mvn -Dserver.port=8181 $@ spring-boot:run
 cd - 
