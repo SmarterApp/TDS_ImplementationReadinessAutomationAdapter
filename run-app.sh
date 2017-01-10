4375b95299ca3957c547d0f8da21d004ec9059ff
Stefan Eng <stefaneng@ucla.edu>
2017-01-10 10:33:31 -0800

    Remove hardcoded irp-package location

--- run-app.sh
+++ run-app.sh
@@ -1,4 +1,4 @@
 source env.sh
 cd irp-automation-adapter-sboss-application
-IRP_PACKAGE_LOCATION=file:///Users/seng/cresst-dev/irp-resources/irp-package mvn -Dserver.port=8181 $@ spring-boot:run
+mvn -Dserver.port=8181 $@ spring-boot:run
 cd - 
