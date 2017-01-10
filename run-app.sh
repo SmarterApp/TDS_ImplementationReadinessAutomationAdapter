9179fbb0e6730bc88b65823409213bd47337b377
Stefan Eng <stefaneng@ucla.edu>
2017-01-09 16:08:26 -0800

    CP-372: use IRP_PACKAGE_LOCATION to configure adapter

--- run-app.sh
+++ run-app.sh
@@ -1,4 +1,4 @@
 source env.sh
 cd irp-automation-adapter-sboss-application
-mvn -Dserver.port=8181 $@ spring-boot:run
+IRP_PACKAGE_LOCATION=file:///Users/seng/cresst-dev/irp-resources/irp-package mvn -Dserver.port=8181 $@ spring-boot:run
 cd - 
